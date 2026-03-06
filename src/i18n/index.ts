d8ebca61492da347bf75668ce3f55388e57f3368
Cristian F. Torres Collazo <cristian@inprende.com>
2026-03-06 13:21:19 -0400

    feat: add multilingual support with English and Spanish translations

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,21 +1,20 @@
-import { initReactI18next } from 'react-i18next';
-
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import HttpBackend from 'i18next-http-backend';
+import { initReactI18next } from 'react-i18next';
+
+import en from './en.json';
+import es from './es.json';
 
 i18n
-  .use(HttpBackend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
+    resources: {
+      es: { translation: es },
+      en: { translation: en },
+    },
     fallbackLng: 'es',
     supportedLngs: ['es', 'en'],
-    ns: ['translation'],
-    defaultNS: 'translation',
-    backend: {
-      loadPath: '/locales/{{lng}}/{{ns}}.json',
-    },
     detection: {
       order: ['localStorage', 'navigator'],
       caches: ['localStorage'],
