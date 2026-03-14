862d66d3506e0d74a998472cf7a926eaf063548f
Cristian F. Torres Collazo <cristian@inprende.com>
2026-03-14 13:11:18 -0400

    chore: comment out testimonials navigation link

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,7 +4,7 @@ export const NAV_LINKS = [
   { key: 'nav.about', href: '#nosotros' },
   { key: 'nav.portfolio', href: '#portafolio' },
   { key: 'nav.certifications', href: '#certificaciones' },
-  { key: 'nav.testimonials', href: '#testimonios' },
+  // { key: 'nav.testimonials', href: '#testimonios' },
   { key: 'nav.contact', href: '#contacto' },
 ] as const;
 
